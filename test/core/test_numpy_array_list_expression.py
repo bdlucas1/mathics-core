312102c28d8929b16282f3de156c57d32e258991
Bruce Lucas <bdlucas1@users.noreply.github.com>
2025-10-11 15:19:50 -0400

    Strengthen test

--- test/core/test_numpy_array_list_expression.py
+++ test/core/test_numpy_array_list_expression.py
@@ -2,8 +2,8 @@ import numpy as np
 
 from mathics.core.list import NumpyArrayListExpression
 
-def test():
 
+def test():
     # compare array a with (nested) list l
     def eq(a, l):
         if isinstance(a, np.ndarray):
@@ -11,19 +11,26 @@ def test():
         else:
             return a.item() == l.to_python()
 
-    def test(a, s):
+    def test(a, a_sum, l_str):
         a = np.array(a)
         l = NumpyArrayListExpression(a)
-        assert not l.is_instantiated, "should not be instantiated immediately after construction"
+        assert np.sum(l.value) == a_sum
+        assert (
+            not l.is_instantiated
+        ), "can create and access value without instantiating"
         assert eq(a, l), "numpy array and instantiated list should be equal"
-        assert l.is_instantiated, "should be instantiated after any use that needs .elements"
-        assert str(l) == s
+        assert (
+            l.is_instantiated
+        ), "should be instantiated after any use that needs .elements"
+        assert str(l) == l_str
 
     # int, float, complex, bool, mixed; empty, 1-d, 2-d
-    test([],                          "{}")
-    test([1, 2, 3],                   "{1,2,3}")
-    test([[1, 2], [3, 4]],            "{{1,2},{3,4}}")
-    test([[17.5, 18.5], [2.2, 3.3]],  "{{17.5,18.5},{2.2,3.3}}")
-    test([17j+3],                     "{3.0 + 17.0*I}")
-    test([True, False],               "{System`True,System`False}")
-    test([True, False, 17, 2.2],      "{1.0,0.0,17.0,2.2}")
+    # fmt: off
+    test([],                         0,       "{}")
+    test([1, 2, 3],                  6,       "{1,2,3}")
+    test([[1, 2], [3, 4]],           10,      "{{1,2},{3,4}}")
+    test([[17.5, 18.5], [2.2, 3.3]], 41.5,    "{{17.5,18.5},{2.2,3.3}}")
+    test([17j + 3],                  17j + 3, "{3.0 + 17.0*I}")
+    test([True, False],              1,       "{System`True,System`False}")
+    test([True, False, 17, 2.2],     20.2,    "{1.0,0.0,17.0,2.2}")
+    # fmt: on
