198c0770cc517b2ecd600fda2f103fad7d61da80
Bruce Lucas <bdlucas1@users.noreply.github.com>
2025-11-06 08:37:29 -0500

    WIP

--- mathics/builtin/numericarray.py
+++ mathics/builtin/numericarray.py
@@ -8,7 +8,7 @@ try:  # pragma: no cover - numpy is optional at runtime
 except ImportError:  # pragma: no cover - handled via requires attribute
     numpy = None
 
-from mathics.core.atoms import NumericArray as NumericArrayAtom, NUMERIC_ARRAY_TYPE_MAP, String
+from mathics.core.atoms import NumericArray, NUMERIC_ARRAY_TYPE_MAP, String
 from mathics.core.builtin import Builtin
 from mathics.core.convert.python import from_python
 from mathics.core.symbols import Symbol, strip_context
@@ -18,7 +18,7 @@ from mathics.core.systemsymbols import SymbolAutomatic, SymbolFailed, SymbolNume
 # name modeled on Complex_ to avoid collision with NumericArray atom
 class NumericArray_(Builtin):
 
-    summary_text = "head for NumericArray"
+    summary_text = "construct NumericArray"
     name = "NumericArray"
     rules = {
         "NumericArray[list_List]": "NumericArray[list, Automatic]"
@@ -52,18 +52,12 @@ class NumericArray_(Builtin):
         # compute array from data and dtype and wrap it in a NumericArray atom
         python_value = data.to_python()
         array = numpy.array(python_value, dtype=dtype)
-        atom = NumericArrayAtom(array, dtype)
+        atom = NumericArray(array, dtype)
 
-        print("xxx eval_list returning", type(atom), atom)
         return atom
 
-    def eval_normal(self, array, evaluation):
-        "System`Normal[array_NumericArray]"
-        print("xxx eval_normal", type(array))
-        return from_python(array.value.tolist())
-
-    # TODO: is this needed or does it happen by default?
-    #def eval_to_string(self, array, evaluation):
-    #    "ToString[array_NumericArray]"
-    #    return String(str(array))
-
+    # this doesn't work
+    # instead see Normal builtin in mathics/builtin/list/constructing.py
+    #def eval_normal(self, array, evaluation):
+    #    "System`Normal[array_NumericArray]"
+    #    return from_python(array.value.tolist())
